ed3f44fca0d63f47436fda241c8fb956783b12d4
Max Rydkin <maks.rydkin@gmail.com>
2012-11-04 23:21:16 +0400

    change place

--- lib/generators/ideyabox/admin/admin_generator.rb
+++ lib/generators/ideyabox/admin/admin_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 module Ideyabox
   module Generators
-    class AdminGenerator < Rails::Generators::Base
+    class AdminGenerator < ::Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
 
       def generate_admin_layout
