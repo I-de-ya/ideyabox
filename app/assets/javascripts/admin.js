864cd29bb36c31767da84f8fb90b27363db374d4
veskakoff <veskakov@gmail.com>
2012-11-06 23:21:05 +0400

    added default

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,12 +1,5 @@
-//= require modernizr.js
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
-//= require swfobject.js
-//= require jquery.uploadify.min
-//= require jquery-ui.min.js
-//= require jquery.ui.nestedSortable
-//= require chosen.jquery.js
+//= require default.js
+//= require_self
 
 $(document).ready(function(){
 
---
+++ app/assets/javascripts/default.js
@@ -0,0 +1,13 @@
+//= require modernizr.js
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require swfobject.js
+//= require jquery.uploadify.min
+//= require jquery-ui.min.js
+//= require jquery.ui.nestedSortable
+//= require chosen.jquery.js
+
+$(document).ready(function(){
+
+});
