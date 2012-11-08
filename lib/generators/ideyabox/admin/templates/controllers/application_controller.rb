50e1bafdef1fb64547d8b6ab9c55b9b53a123f6f
veskakoff <veskakov@gmail.com>
2012-11-08 17:33:04 +0400

    fixed login layout

---
+++ lib/generators/ideyabox/admin/templates/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  layout :layout_by_resource
+  
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
+
+end
